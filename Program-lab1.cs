55bc3d46addadf11a824788478cb6c0c492ede91
StefanMaric99 <57594996+StefanMaric99@users.noreply.github.com>
2020-01-13 11:13:44 -0500

    Add files via upload

---
+++ Program-lab1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnOrderedArray ua = new UnOrderedArray(50);
+            ua.addLast(23);
+            ua.addLast(9);
+            ua.addLast(-2);
+            ua.addLast(98);
+            ua.addLast(3);
+            ua.addLast(1);
+            ua.addLast(8);
+            ua.addLast(75);
+            ua.addLast(-54);
+            ua.addLast(185);
+            ua.listItems();
+            ua.seletionSortAsc();
+            ua.listItems();
+            Console.ReadKey();
+        }
+    }
+}
